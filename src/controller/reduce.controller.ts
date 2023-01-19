e10a8a45e0636bc5581465dedc89276ee1ae1329
Marshall <mn.acunab@gmail.com>
2023-01-19 18:55:36 -0300

    add reduce function

--- src/controller/reduce.controller.ts
+++ src/controller/reduce.controller.ts
@@ -1,14 +1,31 @@
 import { Request, Response } from "express";
+import reduceModel from "../models/reduce.model";
 
 
 class ReduceController {
 
     reduce(req: Request, res: Response) {
 
-        return res.status(200).json({
-            ok: true,
-            msg: 'entro al reduce'
-        });
+        const { url } = req.body;
+
+        const reduce = new reduceModel({
+            originalUrl: url,
+            short: 'aca va el codigo generado por shortuuid'
+        })
+
+        try {
+            //si el short generado no existe 
+            reduce.save();
+            res.status(200).json({
+                ok: true,
+                msg: 'url reduced successfully'
+            });
+        } catch( error ) {
+            res.status(400).json({
+                ok: false,
+                msg: 'an error ocurred while trying reduce the url'
+            });
+        }
     }
 
     getReduce(req: Request, res: Response) {
